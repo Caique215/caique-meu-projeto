f536cc336ee9b4a43f764f37281c99f5ebf9a85e
Caique <caiquesouzaresponsa@hotmail.com>
2021-03-09 18:34:10 -0300

    commit

--- slaide.js
+++ slaide.js
@@ -3,7 +3,7 @@
 const images = [
     { 'id': '1', 'url': 'caique.png'},
     { 'id': '2', 'url': 'caiqueg.jpg'},
-    { 'id': '3', 'url': 'caiqueggg.jpg'},
+    { 'id': '3', 'url': 'caiqueggg.jpg.png'},
     { 'id': '4', 'url': 'caiquegg.jpg'},
     { 'id': '5', 'url': 'caiqueeNN.jpg.jpg'},
    
